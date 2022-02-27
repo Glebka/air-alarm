d693ce788ee419e691661d898df1b1b3b4cd1571
Hlieb Romanov rgebox@gmail.com <rgebox@gmail.com>
2022-02-28 01:02:51 +0200

    Add sound notificcation, add dismiss function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Crawler = require("crawler");
+const play = require("audio-play");
+const load = require("audio-loader");
 
 let lastNewsItemTimestamp = 0;
 let lastNewsItemText = null;
 let alarmEnabled = false;
 
 const disableAlarmKeywords = ["відбій повітряної тривоги"];
-
 const enableAlarmKeywords = ["повітряна тривога", "тривога!"];
 
 function shouldEnableAlarm(text) {
@@ -22,6 +23,35 @@ function shouldEnableAlarm(text) {
     : !!shouldEnable;
 }
 
+function playAudioInLoop() {
+  let playback = null;
+  // https://freesound.org/people/pogmothoin/sounds/351284/
+  load("./notification.wav").then(buf => {
+    playback = play(buf, { loop: true });
+  });
+  return () => {
+    playback.pause();
+  };
+}
+
+// https://stackoverflow.com/questions/19687407/press-any-key-to-continue-in-nodejs
+const keypress = async () => {
+  process.stdin.setRawMode(true);
+  return new Promise(resolve =>
+    process.stdin.once("data", data => {
+      const byteArray = [...data];
+      if (byteArray.length > 0 && byteArray[0] === 3) {
+        console.log("^C");
+        process.exit(1);
+      }
+      process.stdin.setRawMode(false);
+      resolve();
+    })
+  );
+};
+
+let stopPlayback = null;
+
 const c = new Crawler({
   maxConnections: 1,
   forceUTF8: true,
@@ -33,7 +63,6 @@ const c = new Crawler({
       decodeEntities: true
     }
   },
-  // This will be called for each crawled page
   callback: (error, res, done) => {
     if (error) {
       console.error(`${new Date()} > ${error}`);
@@ -62,9 +91,16 @@ const c = new Crawler({
           const requestEnableAlarm = enableOrNot;
           if (!alarmEnabled && requestEnableAlarm) {
             console.log(`${new Date()} > ALARM! Press any key to dismiss.`);
+            if (stopPlayback) {
+              stopPlayback();
+            }
+            stopPlayback = playAudioInLoop();
           } else {
             if (alarmEnabled && !requestEnableAlarm) {
               console.log(`${new Date()} > ALL GOOD NOW :) Disabling alarm`);
+              if (stopPlayback) {
+                stopPlayback();
+              }
             } else {
               console.log(
                 `${new Date()} > Do nothing. alarmEnabled=${alarmEnabled}, requestEnableAlarm=${requestEnableAlarm}`
@@ -85,11 +121,25 @@ const c = new Crawler({
   }
 });
 
+(async () => {
+  while (true) {
+    await keypress();
+    if (alarmEnabled) {
+      alarmEnabled = false;
+      console.log(`${new Date()} > Alarm dismissed by user.`);
+      if (stopPlayback) {
+        stopPlayback();
+      }
+    }
+  }
+})();
+
 c.queue({ uri: "https://omr.gov.ua/", timeout: 5000 });
 
 setInterval(() => {
   c.queue({ uri: "https://omr.gov.ua/", timeout: 5000 });
 }, 1000 * 60 * 3);
+
 
 /** Tests */
 
@@ -133,12 +183,36 @@ setTimeout(() => {
       </div>
       </div>`
   });
-}, 15 * 1000);
-
+}, 3 * 1000);
 
 setTimeout(() => {
     c.queue({
-        html: `<div id="tab1" class="" style="display: block;">
+      html: `<div id="tab1" class="" style="display: block;">
+        <div class="slideshow gallery-js-ready autorotation-disabled">
+            <div class="pagination">
+                <ul>
+                <li>
+                <a href="#">
+                    <time class="date" datetime="2022-02-27 18:50"><i>27 лютого</i> <span>18:50</span></time>
+                    <strong class="title">Увага всім: повітряна тривога! Негайно в укриття</strong>
+                </a>
+            </li>
+            <li>
+                        <a href="#">
+                            <time class="date" datetime="2022-02-27 17:48"><i>27 лютого</i> <span>17:48</span></time>
+                            <strong class="title">Де українці можуть отримати допомогу в Румунії. Фото</strong>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </div>`
+    });
+  }, 15 * 1000);
+
+setTimeout(() => {
+  c.queue({
+    html: `<div id="tab1" class="" style="display: block;">
       <div class="slideshow gallery-js-ready autorotation-disabled">
           <div class="pagination">
               <ul>
@@ -164,6 +238,5 @@ setTimeout(() => {
           </div>
       </div>
       </div>`
-      });
-}, 3 * 60 * 1000);*/
-
+  });
+}, 23 * 1000);*/
